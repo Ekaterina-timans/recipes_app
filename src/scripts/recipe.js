4676245cc9835183696c9114f2121561d0decf5f
Ekaterina-timans <128321173+Ekaterina-timans@users.noreply.github.com>
2024-09-16 14:02:06 +0300

    Changes to the webpack build

---
+++ src/scripts/recipe.js
@@ -0,0 +1,105 @@
+import '../styles/recipe.css';
+import '../styles/style.css';
+import $ from 'jquery';
+
+$(document).ready(function() {
+    initRecipePage();
+    $('.container__btn').on('click', toggleFavoriteRecipe);
+});
+
+function initRecipePage() {
+    const recipeData = getRecipeDataFromStorage();
+    if (recipeData) {
+        displayRecipeData(recipeData);
+        checkFavoriteStatus(recipeData.title);
+        updateIngredientsDisplay();
+    } else {
+        redirectToHome();
+    }
+}
+
+function getRecipeDataFromStorage() {
+    return JSON.parse(localStorage.getItem('selectedRecipe'));
+}
+
+function displayRecipeData(recipeData) {
+    const highResImage = recipeData.image.replace('312x231', '636x393');
+    
+    $('.container__recipe-image').attr('src', highResImage);
+    $('.container__recipe-title').text(recipeData.title);
+    $('.container__time').text(recipeData.totalTime ? `${recipeData.totalTime} minutes` : 'Неизвестно');
+    $('.container__kitchen-title').text(recipeData.cuisineType);
+    $('.container__calories').text(Math.round(recipeData.calories));
+    $('.container__fats').text(recipeData.fats.toFixed(2));
+    $('.container__proteins').text(recipeData.proteins.toFixed(2));
+    $('.container__carbohydrates').text(recipeData.carbohydrates.toFixed(2));
+    $('.container__instructions-text').text(recipeData.instructions);
+    
+    recipeData.ingredients.forEach(displayIngredient);
+}
+
+function displayIngredient(ingredient) {
+    const ingredientImage = ingredient.image.replace('100x100', '250x250');
+    let ingredientHtml;
+    if (window.innerWidth < 624) {
+        ingredientHtml = `<p class="container__ingredient-name">${ingredient.food}</p>`;
+    } else {
+        ingredientHtml = `
+            <div class="container__ingredients-card">
+                <img class="container__ingredients-image" src="${ingredientImage}" alt="${ingredient.food}" width="151" height="115">
+                <p class="container__ingredient">${ingredient.food}</p>
+            </div>
+        `;
+    }
+    $('.container__ingredients-cards').append(ingredientHtml);
+}
+
+function updateIngredientsDisplay() {
+    const recipeData = getRecipeDataFromStorage();
+    if (recipeData) {
+        $('.container__ingredients-cards').empty();
+        recipeData.ingredients.forEach(displayIngredient);
+    }
+}
+
+$(window).on('resize', function() {
+    updateIngredientsDisplay();
+});
+
+function checkFavoriteStatus(title) {
+    const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+    const isFavorite = favoriteRecipes.some(recipe => recipe.title === title);
+
+    if (isFavorite) {
+        $('.container__btn .bxs-heart').addClass('active');
+    } else {
+        $('.container__btn .bxs-heart').removeClass('active');
+    }
+}
+
+function redirectToHome() {
+    window.location.href = 'index.html';
+}
+
+function toggleFavoriteRecipe() {
+    const heartIcon = $(this).find('.bxs-heart');
+    const recipeData = getRecipeDataFromStorage();
+    let favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+
+    if (heartIcon.hasClass('active')) {
+        favoriteRecipes = favoriteRecipes.filter(recipe => recipe.title !== recipeData.title);
+        heartIcon.removeClass('active').addClass('spin');
+    } else {
+        favoriteRecipes.push(recipeData);
+        heartIcon.addClass('active spin');
+    }
+
+    localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipes));
+    setTimeout(() => heartIcon.removeClass('spin'), 1000);
+}
+
+$(document).ready(function() {
+    $('.header__menu').click(function() {
+        $('.header__menu').toggleClass('change');
+    });
+});
---
+++ webpack.config.js
@@ -0,0 +1,100 @@
+const path = require('path');
+const webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        index: './src/scripts/index.js',
+        recipe: './src/scripts/recipe.js',
+        favorites: './src/scripts/favorites.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]'
+                },
+                use: [
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 75,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            webp: {
+                                quality: 75,
+                            },
+                        },
+                    },
+                ],
+            },
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
+        new Dotenv(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            chunks: ['index']
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/recipe.html',
+            filename: 'recipe.html',
+            chunks: ['recipe']
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/favorites.html',
+            filename: 'favorites.html',
+            chunks: ['favorites']
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+    },
+};
